0ecd3267efe6e1cd6b1296569847e29498a60e6d
Artem Vinogradov <vinogradov44@outlook.com>
2023-02-13 20:04:41 +0300

    file encryption

--- decryptfiles.py
+++ decryptfiles.py
@@ -6,6 +6,15 @@ import gnupg
 gpg = gnupg.GPG(gnupghome="C:/Users/vinog/AppData/Roaming/gnupg")
 
 
-def reader_gpg(path: str) -> dict:
-    data = gpg.decrypt_file(path, passphrase=os.getenv('password'))
-    return json.loads(str(data))
+# ToDo finish decrypting files
+def decrypt(path: str) -> dict:
+    encrypt_files = os.listdir(path)
+    for file in encrypt_files:
+        ...
+    print(encrypt_files)
+    # data = gpg.decrypt_file(path, passphrase=os.getenv('password'))
+    # return json.loads(str(data))
+
+
+if __name__ == '__main__':
+    decrypt('./store')
--- encryptfiles.py
+++ encryptfiles.py
@@ -11,8 +11,18 @@ if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
 
-def write_gpg(data: dict):
-    with open("tmp.json", 'w') as f:
-        json.dump(data, f)
-    gpg.encrypt_file(f, recipients=["vinartgen88@yandex.ru"], output='pass.gpg')
-    os.remove("tmp.json")
+def encrypt(name: str, login: str, password: str, url: str):
+    with open(f"{name}.json", 'w') as f:
+        json.dump({
+            f"{name}": {
+                f"{login}": login,
+                f"{password}": password,
+                "url": url
+            }
+        }, f)
+    gpg.encrypt_file(f, recipients=[os.getenv('recipients')], output=f"./store/{name}.gpg")
+    # os.remove("tmp.json")
+
+
+if __name__ == '__main__':
+    encrypt("ya.com", "tv@kmk.ru", "password", "https://mail.ya.com/")
